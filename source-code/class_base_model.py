efd922542d2c0dc0ef5f84cc27b7c783081d9b92
kusin <aryajayaalamsyah@gmail.com>
2024-03-27 05:05:57 +0700

    -

--- source-code/class_base_model.py
+++ source-code/class_base_model.py
@@ -1,7 +1,15 @@
 # library algorithm lstm-rnn with keras
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import RNN
 from keras.layers import LSTM
 from keras.layers import GRU
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import TimeDistributed
+from keras.layers import Bidirectional
+from keras.optimizers import Adam, Adamax, RMSprop, SGD
+from keras.layers import LeakyReLU
 
 # ----------------------------------------------------------------------------------------------------------
 def lstm_algorithm(x_train, activation, dropout_rate, optimizer):
