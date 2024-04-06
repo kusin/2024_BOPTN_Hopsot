c7f66ce4677e3aa3755c38f8e17dff07ff2f2af9
kusin <aryajayaalamsyah@gmail.com>
2024-04-07 00:02:01 +0700

    -

---
+++ source-code/temp/class_model.py
@@ -0,0 +1,13 @@
+# pustaka generate number
+import random as rm
+import time as tm
+
+# pustaka manipulasi data array
+import numpy as np
+
+# pustaka manipulasi data frame
+import pandas as pd
+
+
+def loadData(df):
+    df = pd.read_csv()
