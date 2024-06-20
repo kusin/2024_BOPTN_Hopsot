27e23423dc8bf39fdb363fd9701c3e54d69c7f95
kusin <aryajayaalamsyah@gmail.com>
2024-06-20 07:16:23 +0700

    -

--- C02_visualization.py
+++ C02_visualization.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 # --------------------------------------------------------------
 
 def lineplot1(x, y, label):
-  
-  # membuat frame
+  # membuat time series plot
   fig, ax = plt.subplots(figsize = (10,5))
-  
-  # # membuat time series plot
   ax.plot(x, y, color="tab:blue", label=label, linewidth=2.5)
+
+  # membuat label-label
   ax.set_title("", fontsize=14)
   ax.set_xlabel("", fontsize=12)
   ax.set_ylabel("", fontsize=12)
@@ -20,12 +19,12 @@ def lineplot1(x, y, label):
 # --------------------------------------------------------------
 
 def lineplot2(x1, y1, x2, y2, label1, label2):
-  # membuat frame
+  # membuat time series plot
   fig, ax = plt.subplots(figsize = (10,5))
-  
-  # # membuat time series plot
   ax.plot(x1, y1, color="tab:blue", label=label1, linewidth=2.5)
   ax.plot(x2, y2, color="tab:red", label=label2, linewidth=2.5)
+
+  # membuat label-label
   ax.set_title("", fontsize=14)
   ax.set_xlabel("", fontsize=12)
   ax.set_ylabel("", fontsize=12)
@@ -33,4 +32,38 @@ def lineplot2(x1, y1, x2, y2, label1, label2):
   ax.grid(True)
 
   # return values
+  return plt.show()
+# --------------------------------------------------------------
+
+def lineplot3(training, validation, title):
+  # membuat time series plot
+  fig, ax = plt.subplots(figsize = (8,4))
+  ax.plot(training, color="tab:blue", label="Training", linewidth=2)
+  ax.plot(validation, color="tab:orange", label="Validation", linewidth=2)
+
+  # membuat label-label
+  ax.set_title(title, fontsize=12)#
+  ax.set_xlabel("", fontsize=12)
+  ax.set_ylabel("", fontsize=12)
+  ax.legend(loc="best")
+  ax.grid(True)
+
+  # menampilkan plot
+  return plt.show()
+# --------------------------------------------------------------
+
+def lineplot4(date, ytrue, ypred, title):
+  # membuat time series plot
+  fig, ax = plt.subplots(figsize = (8,4))
+  ax.plot(date, ytrue, color="tab:blue", label="data actual", linewidth=2)
+  ax.plot(date, ypred, color="tab:red", label="results predictions", linewidth=2)
+
+  # membuat label-label
+  ax.set_title(title, fontsize=12)
+  ax.set_xlabel("", fontsize=12)
+  ax.set_ylabel("", fontsize=12)
+  ax.legend(loc="best")
+  ax.grid(True)
+
+  # menampilkan plot
   return plt.show()
--- C04_model_predictions.py
+++ C04_model_predictions.py
@@ -24,17 +24,35 @@ def get_lstm(timestep, activation, dropout, optimizer):
   model.add(Dropout(dropout))
   model.add(Dense(1))
 
-  # 2. Compile models
+  # 2. compile models
   # model.compile(optimizer='adamax', loss='mean_squared_error')
   model.compile(
     optimizer=optimizer,
     loss="mae",
-    metrics=[
-      tf.keras.metrics.MeanAbsoluteError(),
-      tf.keras.metrics.MeanSquaredError(),
-      tf.keras.metrics.MeanAbsolutePercentageError(),
-    ]
+    # metrics=[
+    #   tf.keras.metrics.MeanAbsoluteError(),
+    #   tf.keras.metrics.MeanSquaredError(),
+    #   tf.keras.metrics.MeanAbsolutePercentageError(),
+    # ]
   )
 
   # return values
   return model
+# ----------------------------------------------------------------------------------------
+
+# func model predictions
+def get_predictions(model, x_train, y_train, x_test, y_test, batch_size, epochs):
+  
+  # 4. fitting models
+  history = model.fit(
+    x_train, y_train,
+    batch_size=batch_size, epochs=epochs, verbose=0, 
+    validation_data=(x_test, y_test),
+    use_multiprocessing=False, shuffle=False
+  )
+
+  # 3. predict models
+  predictions = model.predict(x_test, verbose=0)
+
+  # return values
+  return history, predictions
